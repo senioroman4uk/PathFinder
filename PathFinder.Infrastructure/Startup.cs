b9d010d4c4ff8553151275482d0769dc7bd17c46
Vladyslav Romanchuk <senioroman4uk@gmail.com>
2016-04-12 01:53:21 +0300

    Update entry point config

--- PathFinder.Infrastructure/Startup.cs
+++ PathFinder.Infrastructure/Startup.cs
@@ -1,7 +1,7 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Web.Http;
 using Microsoft.Owin;
 using Owin;
+using PathFinder.EntryPoint;
 
 [assembly: OwinStartup(typeof(PathFinder.Infrastructure.Startup))]
 
@@ -12,6 +12,9 @@ namespace PathFinder.Infrastructure
         public void Configuration(IAppBuilder app)
         {
             // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            HttpConfiguration configuration = new HttpConfiguration();
+            WebApiConfig.Register(configuration);
+            app.UseWebApi(configuration);
         }
     }
 }
