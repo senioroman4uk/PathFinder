098c8e9dad1d76da2d048879b9605cd3b1a314ae
Vladyslav Romanchuk <senioroman4uk@gmail.com>
2016-04-12 01:28:55 +0300

    Add JsonFormater

--- PathFinder.EntryPoint/App_Start/WebApiConfig.cs
+++ PathFinder.EntryPoint/App_Start/WebApiConfig.cs
@@ -1,7 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
+using System.Net.Http.Formatting;
 using System.Web.Http;
+using Newtonsoft.Json.Serialization;
 
 namespace PathFinder.EntryPoint
 {
@@ -19,6 +19,9 @@ namespace PathFinder.EntryPoint
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            var jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().First();
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
         }
     }
 }
