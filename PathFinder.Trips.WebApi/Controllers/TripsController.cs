8e64f39aa9975d598d9664dea4b5a0b31ddf2692
Vladyslav Romanchuk <senioroman4uk@gmail.com>
2016-06-01 01:26:24 +0300

    Add action for algorithm testing.

--- PathFinder.Trips.WebApi/Controllers/TripsController.cs
+++ PathFinder.Trips.WebApi/Controllers/TripsController.cs
@@ -4,17 +4,17 @@
 
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
-using System.Web.Http.Results;
 using PathFinder.Trips.DAL.Model;
 using PathFinder.Trips.WebApi.Constants;
 using PathFinder.Trips.WebApi.Extensions;
 using PathFinder.Trips.WebApi.Mappers;
 using PathFinder.Trips.WebApi.Models;
-using PathFinder.Trips.WebApi.Patterns.Factory;
-using PathFinder.Trips.WebApi.Patterns.Strategy;
 using PathFinder.Trips.WebApi.Queries;
 using PathFinder.Trips.WebApi.Services;
 using PathFinder.Trips.WebApi.Validators;
@@ -80,6 +80,70 @@ namespace PathFinder.Trips.WebApi.Controllers
             await _context.SaveChangesAsync();
 
             return Ok(route);
+        }
+
+        [HttpPost]
+        [Route("calculateRoute")]
+        public async Task<IHttpActionResult> CalculateRoute(string algorithm)
+        {
+            if (!Request.Content.IsMimeMultipartContent())
+                return StatusCode(HttpStatusCode.UnsupportedMediaType);
+
+            var provider = new MultipartMemoryStreamProvider();
+            await Request.Content.ReadAsMultipartAsync(provider);
+            if (provider.Contents.Count == 0)
+                return BadRequest("You have to add file with matrix for procesing");
+
+            var parsedMatrix = await GetMatrixAsync(provider);
+            if (!parsedMatrix.Validate())
+                return StatusCode((HttpStatusCode)422);
+
+            var matrix = parsedMatrix.ToSquareMatrix();
+            Route route = _routeService.CalculateRoute(matrix, 0, matrix.GetLength(0) - 1, algorithm);
+            return Ok(route);
+        }
+
+        private static async Task<List<List<double>>> GetMatrixAsync(MultipartMemoryStreamProvider provider)
+        {
+            var file = provider.Contents.First();
+            IList<string> matrixRows = new List<string>();
+            using (var dataStream = await file.ReadAsStreamAsync())
+            using (var reader = new StreamReader(dataStream))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    matrixRows.Add(line);
+                }
+            }
+
+            var parsedMatrix = matrixRows.Select(_ => _.Split().Select(double.Parse).ToList()).ToList();
+            return parsedMatrix;
+        }
+    }
+
+    public static class MatrixMapper
+    {
+        public static bool Validate(this List<List<double>> parsedMatrix)
+        {
+            int length = parsedMatrix.Count;
+
+            return parsedMatrix.All(row => row.Count == length);
+        }
+
+        public static double[,] ToSquareMatrix(this List<List<double>> parsedMatrix)
+        {
+            double[,] matrix = new double[parsedMatrix.Count, parsedMatrix.Count];
+
+            for (int i = 0; i < parsedMatrix.Count; i++)
+            {
+                for (int j = 0; j < parsedMatrix[i].Count; j++)
+                {
+                    matrix[i, j] = parsedMatrix[i][j];
+                }
+            }
+
+            return matrix;
         }
     }
 }
