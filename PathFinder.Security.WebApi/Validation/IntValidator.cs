89e141d70cef8f0aff1812aa5643d7624a29bdf6
IrynaRobulets <irene9531q@gmail.com>
2016-06-03 19:39:11 +0300

    Add IntValidator

---
+++ PathFinder.Security.WebApi/Validation/IntValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PathFinder.Security.WebApi.Validation
+{
+    public class IntValidator : AbstractValidator<int>
+    {
+        public IntValidator()
+        {
+            RuleFor(x => x).NotEmpty().GreaterThan(0);
+        }
+    }
+}
