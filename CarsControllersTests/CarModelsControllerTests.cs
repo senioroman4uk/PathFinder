69d261b8f98d32c127e492d2f89adf5f9c2c3fdc
Vladyslav Romanchuk <senioroman4uk@gmail.com>
2016-06-04 01:25:24 +0300

    Fix new CarModelsQueryController tests

--- CarsControllersTests/CarModelsControllerTests.cs
+++ CarsControllersTests/CarModelsControllerTests.cs
@@ -1,4 +1,7 @@
 ﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
 using System.Web.Http;
 using System.Web.Http.Results;
 using FluentAssertions;
@@ -28,6 +31,43 @@ namespace CarsControllersTests
             }
         }
 
+        public static IEnumerable<object[]> DataForGetModelsByBrandId(int id)
+        {
+            var models = new CarModel[]
+            {
+                new CarModel()
+                {
+                    Id = 1,
+                    BrandId = 1,
+                    Model = "Model 1"
+                },
+                new CarModel()
+                {
+                    Id = 1,
+                    BrandId = 2,
+                    Model = "Model 2"
+                },
+            };
+
+            var brands = new CarBrand[]
+            {
+                new CarBrand()
+                {
+                    Id = 1,
+                    Name = "Name 1",
+                    CarModels = new List<CarModel> { models[0] }
+                },
+                new CarBrand()
+                {
+                    Id = 2,
+                    Name = "Name 2",
+                    CarModels = new List<CarModel> { models[1] }
+                }
+            };
+
+            yield return new object[] { id, models, brands };
+        }
+
         [Theory]
         [MemberData("DataForOkResult")]
         public async void GetModelOkResultById(IEnumerable<CarModel> models)
@@ -46,12 +86,14 @@ namespace CarsControllersTests
         }
 
         [Theory]
-        [MemberData("DataForOkResult")]
-        public async void GetModelOkResultByBrandId(IEnumerable<CarModel> models)
+        [MemberData("DataForGetModelsByBrandId", 1)]
+        public async void GetModelOkResultByBrandId(int id, CarModel[] carModels, CarBrand[] carBrands)
         {
             // Arrange
-            var query = Substitute.For<ICarsContextQuery>();
-            query.CarModels.Returns(new MockForDbSet<CarModel>(models));
+            var builder = new CarContextQueryMockBuilder()
+                .SetCarBrands(carBrands)
+                .SetCarModels(carModels);
+            var query = builder.CarsContextQuery;
             var controller = new CarModelsQueryController(query);
 
             // Act
@@ -59,7 +101,7 @@ namespace CarsControllersTests
 
             // Assert
 
-            result.Should().BeOfType<OkNegotiatedContentResult<CarModelReadModel>>();
+            result.Should().BeOfType<OkNegotiatedContentResult<IEnumerable<CarModelReadModel>>>();
         }
 
         [Theory]
@@ -78,13 +120,13 @@ namespace CarsControllersTests
             result.Should().BeOfType<NotFoundResult>();
         }
 
-        [Theory]
-        [MemberData("DataForOkResult")]
-        public async void GetModelNotFoundResultByBrandId(IEnumerable<CarModel> models)
+        [Fact]
+        public async void GetModelNotFoundResultByBrandId()
         {
             // Arrange
-            var query = Substitute.For<ICarsContextQuery>();
-            query.CarModels.Returns(new MockForDbSet<CarModel>(models));
+            var builder = new CarContextQueryMockBuilder();
+            var query = builder.CarsContextQuery;
+        
             var controller = new CarModelsQueryController(query);
 
             // Act
