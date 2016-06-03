50a1d0fe1acb97e7834520900671a14faaab75c8
Vladyslav Romanchuk <senioroman4uk@gmail.com>
2016-06-04 01:23:53 +0300

    Refactor CarContextMockBuilder with EntityFrameworkTesting.NSubstitute

--- CarsControllersTests/CarContextMockBuilder.cs
+++ CarsControllersTests/CarContextMockBuilder.cs
@@ -1,5 +1,8 @@
 ﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
+using NSubstitute;
 using PathFinder.Cars.DAL.Model;
 using PathFinder.Cars.WebApi.Queries;
 
@@ -7,68 +10,63 @@ namespace CarsControllersTests
 {
     public class CarContextQueryMockBuilder
     {
-        private readonly CarContextMock _fakeQuery;
+        private DbSet<CarBrand> _brands;
+        private DbSet<User> _users;
+        private DbSet<CarModel> _models;
+        private DbSet<Car> _cars;
+        private DbSet<CarColor> _carColors;
 
         public CarContextQueryMockBuilder()
         {
-            _fakeQuery = new CarContextMock();
+            _brands = Substitute.For<DbSet<CarBrand>, IQueryable<CarBrand>, IDbAsyncEnumerable<CarBrand>>().SetupData(new List<CarBrand>());
+            _users = Substitute.For<DbSet<User>, IQueryable<User>, IDbAsyncEnumerable<User>>().SetupData(new List<User>());
+            _models = Substitute.For<DbSet<CarModel>, IQueryable<CarModel>, IDbAsyncEnumerable<CarModel>>().SetupData(new List<CarModel>());
+            _cars = Substitute.For<DbSet<Car>, IQueryable<Car>, IDbAsyncEnumerable<Car>>().SetupData(new List<Car>());
+            _carColors = Substitute.For<DbSet<CarColor>, IQueryable<CarColor>, IDbAsyncEnumerable<CarColor>>().SetupData(new List<CarColor>());
         }
 
         public ICarsContextQuery CarsContextQuery
         {
-            get { return _fakeQuery; }
-        }
+            get
+            {
+                var query =  Substitute.For<ICarsContextQuery>();
+                query.CarBrands.Returns(_brands);
+                query.Users.Returns(_users);
+                query.CarModels.Returns(_models);
+                query.Cars.Returns(_cars);
+                query.CarColors.Returns(_carColors);
 
-        public CarContextQueryMockBuilder SetCars(IEnumerable<Car> cars)
+                return query;
+            }
+        } 
+        public CarContextQueryMockBuilder SetCars(ICollection<Car> cars)
         {
-            _fakeQuery.Cars = new EnumerableQuery<Car>(cars);
+            _cars.SetupData(cars);
             return this;
         }
 
-        public CarContextQueryMockBuilder SetUsers(IEnumerable<User> users)
+        public CarContextQueryMockBuilder SetUsers(ICollection<User> users)
         {
-            _fakeQuery.Users = new EnumerableQuery<User>(users);
+            _users.SetupData(users);
             return this;
         }
 
-        public CarContextQueryMockBuilder SetCarBrands(IEnumerable<CarBrand> carBrands)
+        public CarContextQueryMockBuilder SetCarBrands(ICollection<CarBrand> carBrands)
         {
-            _fakeQuery.CarBrands = new EnumerableQuery<CarBrand>(carBrands);
+            _brands = _brands.SetupData(carBrands);
             return this;
         }
 
-        public CarContextQueryMockBuilder SetCarColors(IEnumerable<CarColor> carColors)
+        public CarContextQueryMockBuilder SetCarColors(ICollection<CarColor> carColors)
         {
-            _fakeQuery.CarColors = new EnumerableQuery<CarColor>(carColors);
+            _carColors = _carColors.SetupData(carColors);
             return this;
         }
 
-        public CarContextQueryMockBuilder SetCarModels(IEnumerable<CarModel> carModels)
+        public CarContextQueryMockBuilder SetCarModels(ICollection<CarModel> carModels)
         {
-            _fakeQuery.CarModels = new EnumerableQuery<CarModel>(carModels);
+            _models.SetupData(carModels);
             return this;
         }
-    }
-
-    public class CarContextMock : ICarsContextQuery
-    {
-        public CarContextMock()
-        {
-            Users = new EnumerableQuery<User>(Enumerable.Empty<User>());
-            CarColors = new EnumerableQuery<CarColor>(Enumerable.Empty<CarColor>());
-            CarBrands = new EnumerableQuery<CarBrand>(Enumerable.Empty<CarBrand>());
-            CarModels = new EnumerableQuery<CarModel>(Enumerable.Empty<CarModel>());
-            Cars = new EnumerableQuery<Car>(Enumerable.Empty<Car>());
-        }
-
-        public IOrderedQueryable<User> Users { get; set; }
-
-        public IOrderedQueryable<CarBrand> CarBrands { get; set;  }
-
-        public IOrderedQueryable<CarColor> CarColors { get; set; }
-
-        public IOrderedQueryable<CarModel> CarModels { get; set; }
-
-        public IOrderedQueryable<Car> Cars { get; set; }
     }
 }
