7efdcb93f282715fdfa67970a99dbf1cdfaa13e3
IrynaRobulets <irene9531q@gmail.com>
2016-06-03 19:53:14 +0300

    Add TripReadModelValidator

---
+++ PathFinder.Trips.WebApi/Validators/TripReadModelValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using PathFinder.Trips.WebApi.Models;
+using System;
+
+namespace PathFinder.Trips.WebApi.Validators
+{
+    public class TripReadModelValidator:AbstractValidator<TripReadModel>
+    {
+        public TripReadModelValidator()
+        {
+            RuleFor(model => model.StartPoint).NotNull();
+            RuleFor(model => model.EndPoint).NotNull();
+            RuleFor(model => model.Algorithm).NotNull();
+            RuleFor(model => model.DepartureDate).NotNull().GreaterThanOrEqualTo(DateTime.Now);
+            RuleFor(model => model.Price).NotEmpty();
+        }
+    }
+}
