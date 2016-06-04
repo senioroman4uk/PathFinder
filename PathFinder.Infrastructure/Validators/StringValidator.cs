ec8e46a86f405093bfd5240d357d91995a2a72a3
Vladyslav Romanchuk <senioroman4uk@gmail.com>
2016-06-05 00:07:07 +0300

    Add string validator

---
+++ PathFinder.Infrastructure/Validators/StringValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace PathFinder.Infrastructure.Validators
+{
+    public class StringValidator : AbstractValidator<string>
+    {
+        public StringValidator()
+        {
+            RuleFor(x => x).Cascade(CascadeMode.StopOnFirstFailure).NotNull().NotEmpty();
+        }
+    }
+}
