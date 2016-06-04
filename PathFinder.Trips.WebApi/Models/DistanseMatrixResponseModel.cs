15560b2852e373f2c1b1b71e456bc8957f42a34a
Vladyslav Romanchuk <senioroman4uk@gmail.com>
2016-06-05 00:36:52 +0300

    Clean comments in DistanseMatrixResponseModel

--- PathFinder.Trips.WebApi/Models/DistanseMatrixResponseModel.cs
+++ PathFinder.Trips.WebApi/Models/DistanseMatrixResponseModel.cs
@@ -8,8 +8,6 @@ namespace PathFinder.Trips.WebApi.Models
 {
     public class DistanseMatrixResponseModel
     {
-//        public List<string> destination_addresses { get; set; }
-//        public List<string> origin_addresses { get; set; }
         public List<Row> Rows { get; set; }
         
         public string Status { get; set; }
