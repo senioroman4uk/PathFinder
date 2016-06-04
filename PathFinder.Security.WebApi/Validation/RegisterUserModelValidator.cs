48f11857af8138fa1e1d414cc7ea77e60d03e461
Vladyslav Romanchuk <senioroman4uk@gmail.com>
2016-06-04 13:58:56 +0300

    Small changes in RegisterUserModelValidator. Add regex for username, Add validation constants

--- PathFinder.Security.WebApi/Validation/RegisterUserModelValidator.cs
+++ PathFinder.Security.WebApi/Validation/RegisterUserModelValidator.cs
@@ -1,4 +1,5 @@
 ﻿using FluentValidation;
+using PathFinder.Security.WebApi.Constants;
 using PathFinder.Security.WebApi.Models;
 
 namespace PathFinder.Security.WebApi.Validation
@@ -10,9 +11,9 @@ namespace PathFinder.Security.WebApi.Validation
             RuleFor(model => model.Email).Cascade(CascadeMode.StopOnFirstFailure).NotNull().NotEmpty().EmailAddress();
             RuleFor(model => model.Password).NotNull();
             RuleFor(model => model.RepeatPassword).NotNull();
-            RuleFor(model => model).Must(ArePasswordEquals).WithMessage("Passwords are not equals");
+            RuleFor(model => model).Must(ArePasswordEquals).WithMessage(ValidationConstants.PasswordsAreNotEqualValidationMessage);
             RuleFor(model => model.LastName).Cascade(CascadeMode.StopOnFirstFailure).NotNull().NotEmpty();
-            RuleFor(model => model.UserName).Cascade(CascadeMode.StopOnFirstFailure).NotNull().NotEmpty();
+            RuleFor(model => model.UserName).Cascade(CascadeMode.StopOnFirstFailure).NotNull().NotEmpty().Matches(@"^[A-Za-z0-9@_]+$");
             RuleFor(model => model.PhoneNumber).Cascade(CascadeMode.StopOnFirstFailure).NotNull().NotEmpty().Matches(@"\+38\([0-9]{3}\)\s[0-9]{3}-[0-9]{2}-[0-9]{2}");
         }
 
