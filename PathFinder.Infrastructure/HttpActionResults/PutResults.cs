eea9be236f20172ebf8f3ca4eb0d21ec4031ac8d
Vladyslav Romanchuk <senioroman4uk@gmail.com>
2016-06-11 00:11:40 +0300

    7: Add PutResults helper method

---
+++ PathFinder.Infrastructure/HttpActionResults/PutResults.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+using System.Web.Http;
+using System.Web.Http.Results;
+using PathFinder.Infrastructure.Extensions;
+
+namespace PathFinder.Infrastructure.HttpActionResults
+{
+    public static class PutResults
+    {
+        public static IHttpActionResult Accepted(ApiController controller)
+        {
+            controller.CheckNotNull();
+            return new StatusCodeResult(HttpStatusCode.Accepted, controller);
+        }
+
+        public static IHttpActionResult Accepted<TModel>(ApiController controller, TModel model)
+        {
+            controller.CheckNotNull();
+            return new NegotiatedContentResult<TModel>(HttpStatusCode.Accepted, model, controller);
+        }
+    }
+}
